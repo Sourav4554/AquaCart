dbe7190d49246a5a99ecc60a502ecbb26c2d23db
sourav <souravkrishna358@gmail.com>
2025-01-12 10:57:41 +0530

    return the accidently deleted home.jsx file and add some changes in card .css file

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../../Component/Header/Header'
-import About from '../../Component/About/About'
+import About  from '../../Component/About/About'
 import Promotion from '../../Component/Promotion/Promotion'
 import Gallery from '../../Component/Gallery/Gallery'
 import AiPromotion from '../../Component/AiPromotion/AiPromotion'
@@ -8,12 +8,12 @@ import NewCollection from '../../Component/NewCollection/NewCollection'
 const Home = () => {
   return (
     <div>
-        <Header/>
-        <About/>
-        <Promotion/>
-        <Gallery/>
-        <AiPromotion/>
-         <NewCollection/> 
+      <Header/>
+      <About/>
+      <Promotion/>
+      <Gallery/>
+      <AiPromotion/>
+      <NewCollection/>
     </div>
   )
 }
