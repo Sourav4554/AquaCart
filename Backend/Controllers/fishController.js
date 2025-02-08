1ab3f9258de149e9588912dd775d98279befa651
sourav <souravkrishna358@gmail.com>
2025-02-08 16:35:26 +0530

    implement delete fish in backend

--- Backend/Controllers/fishController.js
+++ Backend/Controllers/fishController.js
@@ -33,13 +33,34 @@ return res.status(200).json({succes:true,message:'fish added'})
 }
 }
 
-//controller for list food item 
+//controller for list fish
 const listFish=async(req,res)=>{
 try {
     const fishList=await fishModel.find({})
     return res.status(200).json({succes:true,message:fishList})
 } catch (error) {
-    return res.status(400).json({succes:false,message:"Internal server error"})
+    return res.status(500).json({succes:false,message:"Internal server error"})
 }
 }
-export{addFish,listFish}
+
+//controller for delete fish
+const deleteFish=async(req,res)=>{
+const {productId}=req.body;
+try {
+   const product=await fishModel.findById(productId)
+   if(!product){
+    return res.status(400).json({succes:false,message:"product not found"})
+    }else{
+    const imageUrl=product.image;
+    const publicId = imageUrl.replace(/^.*\/upload\/v\d+\//, "").replace(/\.[^.]+$/, "");
+    await cloudinary.uploader.destroy(publicId)
+    await fishModel.findByIdAndDelete(productId);
+    return res.status(200).json({succes:true,message:"product removed"})
+    }
+} catch (error) {
+    console.log(error)
+    return res.status(500).json({succes:false,message:"Internal server error"})
+   
+}
+}
+export{addFish,listFish,deleteFish}
--- Backend/Routers/fishRouter.js
+++ Backend/Routers/fishRouter.js
@@ -1,8 +1,9 @@
 import epress from 'express'
 import { upload } from '../Middlewares/uploadFile.js';
-import { addFish, listFish } from '../Controllers/fishController.js';
+import { addFish, deleteFish, listFish } from '../Controllers/fishController.js';
 const fishRouter=epress.Router();
 
-fishRouter.post('/upload',upload.single('image'),addFish)
-fishRouter.get('/list-fish',listFish)
+fishRouter.post('/upload',upload.single('image'),addFish);
+fishRouter.get('/list-fish',listFish);
+fishRouter.delete('/remove-fish',deleteFish);
 export default fishRouter;
