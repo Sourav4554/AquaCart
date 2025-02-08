91057104a3f178f239414bb5c80a30e1956eaa80
sourav <souravkrishna358@gmail.com>
2025-02-08 15:39:53 +0530

    change review in fish schema

--- Backend/Models/fishModel.js
+++ Backend/Models/fishModel.js
@@ -34,10 +34,12 @@ stock:{
     default:true,
     required:true,
 },
-review:{
-    type:Object,
-    default:{}
-}
+review:[{
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    comment: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+}]
 },{minimize:false ,timestamps:true})
 
 const fishModel=mongoose.models.fish || mongoose.model('fish',fishSchema);
